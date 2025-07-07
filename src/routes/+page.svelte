2288179cab1133aacb744ea14ff45585815ea37c
Nareg.T <nareg23@gmail.com>
2025-07-06 21:12:34 -0400

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	let name = $state('');
+	let email = $state('');
+
+	function handleSubmit(event: Event) {
+		event.preventDefault();
+		alert(`Name: ${name}\nEmail: ${email}`);
+	}
+</script>
+
+<form onsubmit={handleSubmit}>
+	<label>
+		Name:
+		<input type="text" bind:value={name} required />
+	</label>
+	<br />
+	<label>
+		Email:
+		<input type="email" bind:value={email} required />
+	</label>
+	<br />
+	<button type="submit">Submit</button>
+</form>
